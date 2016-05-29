2a554528c16dab6f7794210dcc777356c39a1abe
Matthew Yefima <matt.yefima@gmail.com>
2016-05-28 23:34:37 -0700

    Check for existence of instrument choices before reading files

--- AlgoComp/AlgoComp/AlgoComp.cpp
+++ AlgoComp/AlgoComp/AlgoComp.cpp
@@ -10,19 +10,19 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-
+#include <vector>
 //the number of selections available for each instrument in each portion of the song
 const int INTRO_BASS = 1;
 const int INTRO_SNARE = 1;
 const int INTRO_CRASH = 1;
 
-const int VERSE_BASS = 1;
-const int VERSE_SNARE = 1;
-const int VERSE_CRASH = 1;
+const int VERSE_BASS = 0;
+const int VERSE_SNARE = 0;
+const int VERSE_CRASH = 0;
 
-const int CHORUS_BASS = 1;
-const int CHORUS_SNARE = 1;
-const int CHORUS_CRASH = 1;
+const int CHORUS_BASS = 0;
+const int CHORUS_SNARE = 0;
+const int CHORUS_CRASH = 0;
 
 //enum for composing a song structure
 //used to index 1st dimension of availableTracks
@@ -44,8 +44,7 @@ const int availableTracks[3][3] = { {INTRO_BASS,	INTRO_SNARE,	INTRO_CRASH},
 								  };
 
 
-//Assume 120bpm tempo in 4/4 time -> 2000ms per measure
-//use to update the base time of the current measure
+//Assumes 120 ticks per quarter note as default
 int barToTicks(int bar) {
 	return bar * 480;
 }
@@ -58,13 +57,13 @@ int barToTicks(int bar) {
 void appendBar(const MidiFile& input, MidiFile& output, const int& bar) {
 	MidiFile in = input;
 	int count = in.getEventCount(1);
-	std::cout << count << std::endl;
 	for (int i = 0; i < count; ++i) {
 		MidiEvent me = in.getEvent(1, i);
-		me.tick = me.tick / 4 + barToTicks(bar);
+		me.tick = me.tick / 4 + barToTicks(bar); // / 4 is a magic number~
 		output.addEvent(me);
 	}
 }
+
 //Appends an entire section of a song ie verse, chorus, etc.
 //basePath - the location of the files to randomly select instruments from
 //available - an array containing the number of options available for that instrument in this song section
@@ -72,19 +71,32 @@ void appendBar(const MidiFile& input, MidiFile& output, const int& bar) {
 //bar - the current bar of the song
 //output - the destination MidiFile
 void appendSongSection(std::string basePath, const int* available, int barsToAdd, int& bar,  MidiFile& output ) {
-	MidiFile bass, snare, crash;
-	bass.read(basePath	+ "bass"  + std::to_string((std::rand() % available[BASS]) + 1)  + ".mid");
-	snare.read(basePath + "snare" + std::to_string((std::rand() % available[SNARE]) + 1) + ".mid");
-	crash.read(basePath + "crash" + std::to_string((std::rand() % available[CRASH]) + 1) + ".mid");
+	MidiFile snare, crash;
+	std::vector<MidiFile> instruments;
+
+	if (available[BASS] > 0) {
+		MidiFile bass;
+		bass.read(basePath + "bass" + std::to_string((std::rand() % available[BASS]) + 1) + ".mid");
+		instruments.push_back(bass);
+	}
+	if (available[SNARE] > 0) {
+		MidiFile snare;
+		snare.read(basePath + "snare" + std::to_string((std::rand() % available[SNARE]) + 1) + ".mid");
+		instruments.push_back(snare);
+	}
+	if (available[CRASH] > 0) {
+		MidiFile crash;
+		crash.read(basePath + "crash" + std::to_string((std::rand() % available[CRASH]) + 1) + ".mid");
+		instruments.push_back(crash);
+	}
 	
-	bass.setTicksPerQuarterNote(output.getTicksPerQuarterNote());
-	snare.setTicksPerQuarterNote(output.getTicksPerQuarterNote());
-	crash.setTicksPerQuarterNote(output.getTicksPerQuarterNote());
+	//make sure all tracks use same ticks per quarter note as output file
+	for (MidiFile i : instruments)
+		i.setTicksPerQuarterNote(output.getTicksPerQuarterNote());
 
 	for (int i = 0; i < barsToAdd; ++i) {
-		appendBar(bass, output, bar);
-		appendBar(snare, output, bar);
-		appendBar(crash, output, bar);
+		for (MidiFile i : instruments)
+			appendBar(i, output, bar);
 		++bar;
 	}
 }
